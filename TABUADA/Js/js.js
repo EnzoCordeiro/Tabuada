629a9b7901ee30a3142a14c0c3206562ce021796
EnzoCordeiro <105719804+EnzoCordeiro@users.noreply.github.com>
2022-06-01 11:59:28 -0300

    Add files via upload

---
+++ TABUADA/Js/js.js
@@ -0,0 +1,12 @@
+fuction tabuasa(){
+    var num = parset (document.getElementById("num").value);
+    var resultado = document.getElementById('respota');
+    var tabuada = '';
+
+    for (var conut = 1; count <=10; count ++){
+        tabuada += num + "X" + COUNT + "=" +
+        num*count+ "<br/>";
+
+        resultado.innerHTML = tabuada:
+    }
+}
